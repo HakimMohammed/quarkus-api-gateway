3c29f099c4cb0b1a774c2ba7fa2f9262a4dc5e88
HakimMohammed <mohamed.hakim.dev@gmail.com>
2026-02-03 09:58:30 +0100

    Product Service: Used DATA Resource to create a testing app

---
+++ product-service/src/main/java/com/example/Product.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Product extends PanacheEntity {
+    public String name;
+    public double price;
+    public String description;
+}
